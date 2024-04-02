024cdb56519e300c99c691f4257d44aacf5eda56
zhou <zhou@chuyu.com>
2024-04-02 22:41:42 +0800

    init

---
+++ src/main/java/com/littlemonster/chushiwiki/ChushiWikiApplication.java
@@ -0,0 +1,13 @@
+package com.littlemonster.chushiwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChushiWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChushiWikiApplication.class, args);
+    }
+
+}
