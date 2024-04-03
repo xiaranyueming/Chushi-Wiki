c1f505e843b6c27d5126d683073662556a2b4f71
zhou <zhou@chuyu.com>
2024-04-03 11:37:50 +0800

    config 跨域配置

---
+++ src/main/java/com/littlemonster/chushiwiki/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.littlemonster.chushiwiki.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author: white-zhou
+ * @date: 2024-04-03 11:36
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+
+
+
+    // 跨域配置
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("*")
+                .allowCredentials(true)
+                .maxAge(3600)
+                .allowedHeaders("*");
+    }
+}
